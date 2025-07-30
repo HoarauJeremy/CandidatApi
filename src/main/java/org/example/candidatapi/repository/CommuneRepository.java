7b82afa82039bbebb962c6678ebbe302d90fa567
jeremy <jeremyhoarau44@gmail.com>
2025-07-30 20:16:06 +0400

    Renommage de CommunesRepository en CommuneRepository

--- src/main/java/org/example/candidatapi/repository/CommuneRepository.java
+++ src/main/java/org/example/candidatapi/repository/CommuneRepository.java
@@ -2,9 +2,8 @@ package org.example.candidatapi.repository;
 
 import org.example.candidatapi.entity.Commune;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommunesRepository extends JpaRepository<Commune, Long> {
+public interface CommuneRepository extends JpaRepository<Commune, Long> {
 }
